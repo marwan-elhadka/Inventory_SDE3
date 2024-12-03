0b3be7b837b6ea9d86b5434c4f664ec34f34abac
Omar Abseh <omar_absah@icloud.com>
2024-12-03 13:21:01 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module sde.group.iii.inventory_sde3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens sde.group.iii.inventory_sde3 to javafx.fxml;
+    exports sde.group.iii.inventory_sde3;
+}
